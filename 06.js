b75386eaf7988e14034ac340f2f128c5397e89f6
anandia <anandiamy@gmail.com>
2020-03-23 15:26:42 +0700

    no 6

--- 06.js
+++ 06.js
@@ -1,22 +1,19 @@
 function generateCensor(censoredWord) {
-    let result = '';
-    for (let word in censoredWord.split('')) {
+    var result = '';
+    for (var word in censoredWord.split('')) {
         result += 'x';
     }
-
     return result;
 }
-
-function censoringWords(words: string): string {
-    let censoredWords = ['dolor', 'elit', 'quis', 'nisi', 'fugiat', 'proident', 'laborum'];
-    for (let censoredWord of censoredWords) {
+function censoringWords(words) {
+    var censoredWords = ['dolor', 'elit', 'quis', 'nisi', 'fugiat', 'proident', 'laborum'];
+    for (var _i = 0, censoredWords_1 = censoredWords; _i < censoredWords_1.length; _i++) {
+        var censoredWord = censoredWords_1[_i];
         words = words.replace(censoredWord, generateCensor(censoredWord));
     }
-
     return words;
 }
-
 console.log(censoringWords('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.\n' +
     'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.\n' +
     'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.\n' +
-    'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'))
+    'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'));
